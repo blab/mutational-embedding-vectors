91fec5362bb7ee61f557c1690dcff1892b1acdab
RoseyGreenBlue <djhaayushv4@gmail.com>
2026-07-24 20:34:02 +0000

    changing order or setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 export covfit_stuff_path="./notebooks/plm_circuits/covfit_stuff"
 
+echo "Downloading data"
+snakemake --cores all all_pathogens
+
 echo "downloading covfit"
 echo "models will be located in ${covfit_stuff_path}"
 wget  -P ${covfit_stuff_path} "https://zenodo.org/records/14438178/files/covfit_cli_20241007.tar.gz"
@@ -8,6 +11,4 @@ rm covfit_cli_20241007.tar.gz
 cp -r ${covfit_stuff_path}/CoVFit_CLI/_internal/files/models ${covfit_stuff_path}
 rm -rf ${covfit_stuff_path}/CoVFit_CLI
 
-echo "Downloading data"
-snakemake --cores all all_pathogens
 
