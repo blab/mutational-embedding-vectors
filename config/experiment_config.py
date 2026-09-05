75b340c479eff17d63c221c2137ceebc6ff39de3
RoseyGreenBlue <djhaayushv04@gmail.com>
2026-09-05 21:23:35 +0000

    updating config

--- config/experiment_config.py
+++ config/experiment_config.py
@@ -1,1 +1,28 @@
+import os
+
+curr_directory_path = os.path.dirname(os.path.realpath(__file__))
+
+# important global variables
+FOLD_ID = 0
+N_TARGETS = 1565
+CONTEXT_LEN = 1024
+
+# global paths (used for setup of both CovFit and ESM_coronaviridae)
+COVFIT_STUFF_PATH = os.path.join(curr_directory_path, "../notebooks/covfit_stuff")
+MODEL_DIR = os.path.join(COVFIT_STUFF_PATH, "models")
+TOK_DIR = os.path.join(COVFIT_STUFF_PATH, "Tokenizer")
+TASK_IDS_FILE = os.path.join(COVFIT_STUFF_PATH, "task_id_dict.pt")
+
+
+# coronaviridae path
+MODEL_PATH = {
+    "coronaviridae":os.path.join(MODEL_DIR, "model_ESM2_coronaviridae/pytorch_model.bin"),
+    "covfit":os.path.join(MODEL_DIR, f"covfit_model_20241007_{FOLD_ID}.ckpt")
+}
+CONF_PATH = {
+    "coronaviridae":os.path.join(MODEL_DIR, "model_ESM2_coronaviridae/config.json"),
+}
+
+f"./covfit_stuff/models/covfit_model_20241007_{FOLD_ID}.ckpt"
+
 device = "cuda"
--- setup.sh
+++ setup.sh
@@ -1,28 +1,28 @@
 export covfit_stuff_path="./notebooks/covfit_stuff"
 
-# echo "installying ipykernel (make sure you're already in exploratory_env conda environment!!)"
-# conda install ipykernel
-# ipython kernel install --user --name="exploratory_env"
-# 
-# echo "creating figure directory (if not already created)"
-# mkdir figures
-# 
-# echo "installing nextstrain"
-# curl -fsSL --proto '=https' https://nextstrain.org/cli/installer/linux | bash
-# printf '\n%s\n' 'eval "$("/home/ubuntu/.nextstrain/cli-standalone/nextstrain" init-shell bash)"' >> ~/.bashrc
-# eval "$("/home/ubuntu/.nextstrain/cli-standalone/nextstrain" init-shell bash)"
-# python3 -m pip install nextstrain-augur
-# 
-# echo "installing data"
-# wget -O ./notebooks/covfit_stuff/task_id_dict.pt "https://huggingface.co/TheSatoLab-UTokyo/CoVFit/resolve/main/task_id_dict.pt"
-# echo "Downloading covid data"
-# snakemake --cores all data/pathogen/{"sars_cov_2_spike","sars_cov_2_spike_asia","sars_cov_2_spike_africa","sars_cov_2_spike_europe","sars_cov_2_spike_north_america","sars_cov_2_spike_oceania","sars_cov_2_spike_south_america"}/branches.tsv
-# 
-# echo "installing data for OOD steering"
-# mkdir "./data/steering"
-# nextstrain remote download https://nextstrain.org/nextclade/sars-cov-2 "./data/steering/steering_data.json"
-# python ./scripts/alignment.py --json "./data/steering/steering_data.json" --output "./data/steering/lineages.fasta" --gene S --tips-only
-# wget -O "./data/steering/parent_child_fitness.tsv" "https://drive.google.com/uc?export=download&id=1vvYmwZC2Sn6Ivi_y9G65i3wB5d4V8nUs"
+echo "installying ipykernel (make sure you're already in exploratory_env conda environment!!)"
+conda install ipykernel
+ipython kernel install --user --name="exploratory_env"
+
+echo "creating figure directory (if not already created)"
+mkdir figures
+
+echo "installing nextstrain"
+curl -fsSL --proto '=https' https://nextstrain.org/cli/installer/linux | bash
+printf '\n%s\n' 'eval "$("/home/ubuntu/.nextstrain/cli-standalone/nextstrain" init-shell bash)"' >> ~/.bashrc
+eval "$("/home/ubuntu/.nextstrain/cli-standalone/nextstrain" init-shell bash)"
+python3 -m pip install nextstrain-augur
+
+echo "installing data"
+wget -O ./notebooks/covfit_stuff/task_id_dict.pt "https://huggingface.co/TheSatoLab-UTokyo/CoVFit/resolve/main/task_id_dict.pt"
+echo "Downloading covid data"
+snakemake --cores all data/pathogen/{"sars_cov_2_spike","sars_cov_2_spike_asia","sars_cov_2_spike_africa","sars_cov_2_spike_europe","sars_cov_2_spike_north_america","sars_cov_2_spike_oceania","sars_cov_2_spike_south_america"}/branches.tsv
+
+echo "installing data for OOD steering"
+mkdir "./data/steering"
+nextstrain remote download https://nextstrain.org/nextclade/sars-cov-2 "./data/steering/steering_data.json"
+python ./scripts/alignment.py --json "./data/steering/steering_data.json" --output "./data/steering/lineages.fasta" --gene S --tips-only
+wget -O "./data/steering/parent_child_fitness.tsv" "https://drive.google.com/uc?export=download&id=1vvYmwZC2Sn6Ivi_y9G65i3wB5d4V8nUs"
 
 echo "downloading covfit"
 echo "models will be located in ${covfit_stuff_path}/models"
@@ -33,7 +33,7 @@ tar -xf ${covfit_stuff_path}/covfit_cli_20241007.tar.gz -C ${covfit_stuff_path}
 cp -r "${covfit_stuff_path}/CoVFit_CLI/_internal/files/models/"* "${covfit_stuff_path}/models/"
 # rm -rf ${covfit_stuff_path}/CoVFit_CLI
 
-# echo "downloading ESM2_coronaviridae"
-# wget  -O "${covfit_stuff_path}/models/ESM2_coronaviridae.tar.gz" "https://zenodo.org/records/10910360/files/model_ESM2_coronaviridae.tar.gz"
-# tar -xf "${covfit_stuff_path}/models/ESM2_coronaviridae.tar.gz" -C "${covfit_stuff_path}/models/"
-# rm "${covfit_stuff_path}/models/ESM2_coronaviridae.tar.gz"
+echo "downloading ESM2_coronaviridae"
+wget  -O "${covfit_stuff_path}/models/ESM2_coronaviridae.tar.gz" "https://zenodo.org/records/10910360/files/model_ESM2_coronaviridae.tar.gz"
+tar -xf "${covfit_stuff_path}/models/ESM2_coronaviridae.tar.gz" -C "${covfit_stuff_path}/models/"
+rm "${covfit_stuff_path}/models/ESM2_coronaviridae.tar.gz"
